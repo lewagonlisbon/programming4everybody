b5a6684bd832c1df850469bb3a448625ae0173aa
Gabriele Canepa <git@gabrielecanepa.com>
2020-04-04 23:35:54 +0200

    Add ruby overview to day 1

--- 01_introduction.rb
+++ 01_introduction.rb
@@ -1,1 +1,10 @@
-# Let's play with Ruby!
+# Overview of *Ruby*:
+# - Created by "Matz" in 1995
+# - Super easy to pick up, @yukihiro_matz wanted a language that emphasized human needs over those of computers, and
+#   makes Ruby as close as possible to plain english
+# - 10 years later, in 2005, @dhh creates Ruby on Rails, an incredibly powerful framework ("extension" of a language)
+#   to build applications, used nowadays by a lot of companies (including Github, Codecademy, Airbnb, and many more)
+# - At Le Wagon we teach Ruby and Rails and how to build any web app you can think of! This course is just Ruby though,
+#   but you can read more and apply at https://lewagon.com
+#
+# Let's see Ruby in action!
