82fe19f770d8eeaf54795c9667cda3d5ec5251b8
Gabriele Canepa <git@gabrielecanepa.com>
2020-04-27 13:51:04 +0200

    Upload 09_oop slides and initial example

--- 09_oop.rb
+++ 09_oop.rb
@@ -1,1 +1,25 @@
-# DON'T EDIT OR REMOVE THIS FILE! This session will start Monday, April 27th at 13 (GMT+1). Always check Slack ;)
+# **OOP = Object-Oriented Programming**
+# Ruby is an *object-oriented language*
+# As any other OOP language, it uses **objects** to store *attributes* and **methods** to store *behaviours*
+
+# ATTRIBUTES = Objects
+# +
+# BEHAVIOURS = Methods
+
+"Hello!".length # => the *method* length returns the *attribute* `6` of type `Integer`
+4.even? # => returns `true`, of type `TrueClass`
+teachers = [
+  {
+    name: "Mariana",
+    age: 37,
+    country: "🇵🇹"
+  },
+  {
+    name: "Gabriele",
+    age: 27,
+    country: "🇮🇹"
+  }
+] # => returns an `Hash`
+get_flag = ->(teacher) { teacher[:country] } # another way to assign a `Proc` (a *block* of code contained in `def`-`end` or `{`-`}`)
+flags = teachers.map(&get_flag) # pass the `Proc` to the method, returns an `Array` with 2 `String`s
+puts flags # returns `nil` (`NilClass`) and sends the attribute to the console
